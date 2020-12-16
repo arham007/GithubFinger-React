af6debc50ea34119189024dc2db6778f1a8133eb
ArhamKhan <arhamkhancs99@gmail.com>
2020-12-17 01:45:54 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import logo from './bg-main.jpg';
 import './App.css';
 
 function App() {
+
+  console.log(logo)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   <div className="mainDiv">
+     <div className="first_Div">
+       <p>UserFinder for <br /> <span>github</span></p>
+     </div>
+     
+
+
+   </div>
+   </>
   );
 }
 
