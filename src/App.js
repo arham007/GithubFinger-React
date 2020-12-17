ac7cd5e7e86d904b4951e09a9afa3c774d5b98de
ArhamKhan <arhamkhancs99@gmail.com>
2020-12-17 17:00:18 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
+import { useEffect, useState } from 'react';
 import logo from './bg-main.jpg';
-import './App.css';
+import './index.css';
+import Data from './Data';
+
 
 function App() {
+  let [final,setFinal]=useState("");
+  let [name,setName]=useState("");
+
+  const inputEvent=(event)=>{
+    setFinal(event.target.value);
+
+  }
+  const submit=()=>{
+      setName(final)
+  }
+
 
   console.log(logo)
   return (
    <>
-   <div className="mainDiv">
-     <div className="first_Div">
-       <p>UserFinder for <br /> <span>github</span></p>
-     </div>
-     
-    <div className="changing">
-      <div>
-        <input type="text" />
+      <div className="mainDiv">
+        
+
+        <div className="first_Div">
+          <div data-aos="fade-up" data-aos-anchor-placement="center-center" data-aos-duration="2000">
+
+            <p>UserFinder for <br /> <span className="span">github</span></p>
+          </div>
+
+
+
+        </div>
+        <div className="dummyPara">
+          <p>Check out the repos, followers, and more,<br />
+       just<br />by entering a username.</p>
+
+        </div>
+
+        <div className="changing">
+          <div >
+            <input type="text" placeholder="Enter username" onChange={inputEvent} />
+            <div>
+
+              <button onClick={submit}>Submit</button>
+            </div>
+          </div>
+
+        </div>
+        
+      
+
+
       </div>
+    
+    { name==="" ? null
+    :   
+     <Data asname={name}  />}
+   
+      
+ 
+ 
+ 
 
-    </div>
-
-   </div>
-   </>
-  );
+ </>
+ );
 }
 
 export default App;
